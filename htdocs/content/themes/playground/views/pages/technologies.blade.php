2ff3001d2b29da05a0b3805e500057e38c71c397
Darylabrador <abrador.daryl@gmail.com>
2021-02-25 23:27:20 +0400

    Finish apply wireframe

--- htdocs/content/themes/playground/views/pages/technologies.blade.php
+++ htdocs/content/themes/playground/views/pages/technologies.blade.php
@@ -9,7 +9,7 @@
             <div class="flex flex-wrap -mx-1 lg:-mx-4">
             @foreach ($dataPost as $item)
                 <div class="my-1 px-1 w-full md:w-1/2 lg:my-4 lg:px-4 lg:w-1/4">
-                    <article class="overflow-hidden rounded-lg shadow-lg bg-gray-700">
+                    <article class="overflow-hidden rounded-lg shadow-lg bg-gray-700 h-80" style="background-image: url('{!! get_the_post_thumbnail_url (($item->ID), 'large') !!}'); background-size: cover;">
                         <header class="flex items-center justify-center leading-tight p-2 md:p-4">
                             <h1 class="text-lg text-center">
                                 <a class="text-white hover:text-white visited:text-white focus:text-white" href="/{{ $item->post_type }}/{{ $item->post_name }}">
@@ -17,7 +17,7 @@
                                 </a>
                             </h1>
                         </header>
-                        <footer class="flex flex-wrap items-center justify-center leading-none p-2 md:p-4">
+                        <footer class="flex flex-wrap h-4/6 content-end justify-center leading-none p-2 md:p-4">
                             <a href="/{{ $item->post_type }}/{{ $item->post_name }}" class="rounded-full bg-gray-500 text-white mx-2 px-4 py-1 hover:bg-gray-900 hover:text-white visited:text-white focus:text-white"> Détails  </a>
                         </footer>
                     </article>
