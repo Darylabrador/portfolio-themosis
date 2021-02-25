74606621c8f282a07837c5a5b18e068c5ff7f0fc
Darylabrador <abrador.daryl@gmail.com>
2021-02-25 23:40:00 +0400

    Modify footer

--- htdocs/content/themes/playground/views/layouts/footer.blade.php
+++ htdocs/content/themes/playground/views/layouts/footer.blade.php
@@ -1,5 +1,5 @@
 <footer class="bg-gray-800" style="padding: 5px 0;">
     <div class="text-center text-white text-opacity-60">
-        Copyright Daryl ABRADOR
+        Copyright © Daryl ABRADOR
     </div>
 </footer>
